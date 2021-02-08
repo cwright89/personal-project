bce1d6b7f0cf2a4cd5b49aaaef93ab17f7cea7b3
cwright89 <connorwwright1989@gmail.com>
2021-02-08 11:44:20 -0700

    changed some stuff in profile.  just need to finish S3, styling and hosting and i'm done

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -8,6 +8,7 @@ class Profile extends Component {
     constructor(props){
         super(props)
         this.state = {
+            profilePicture: '',
             username: '',
             editView: false
         }
@@ -31,6 +32,21 @@ class Profile extends Component {
             .catch(err => console.log(err))
     }
 
+    handleProfilePicInput = (val) => {
+      this.setState({profilePicture: val})
+  }
+
+    updateProfilePic = () => {
+      axios.put(`/api/user/${this.props.users.user_id}`, { profilePicture: this.state.profilePicture })
+          .then(res => {
+              this.props.getUser(res.data[0])
+              this.handleEditView()
+              this.setState({profilePicture: ''})
+          })
+          .catch(err => console.log(err))
+  }
+  
+
     handleLogout = () => {
         axios.get('/api/logout')
           .then(() => {
@@ -48,11 +64,28 @@ class Profile extends Component {
                     className='profile-picture'
                     src={this.props.user.profile_picture}
                     alt={this.props.user.username} />
+                    {!this.state.editView
+                  ? (
+                    <div>
+                      <button onClick={this.handleEditView}>Edit Profile Picture</button>
+          
+                    </div>
+                  )
+                  : (
+                    <div>
+                      <input
+                        value={this.state.profilePicture}
+                        placeholder='New Profile Pic'
+                        onChange={e => this.handleProfilePicInput(e.target.value)} />
+                      <button onClick={this.updateProfilePic} id='edit-btn'>Submit</button>
+                    </div>
+                  )}
                 {!this.state.editView
                   ? (
                     <div>
                       <h2>{this.props.user.username}</h2>
                       <button onClick={this.handleEditView}>Edit Username</button>
+          
                     </div>
                   )
                   : (
