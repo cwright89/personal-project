5c2da63099b4a95824830a9dfa0a51b5ab77b563
cwright89 <connorwwright1989@gmail.com>
2021-02-11 17:04:03 -0700

    testing error

--- server/index.js
+++ server/index.js
@@ -59,6 +59,7 @@ const contactEmail = nodemailer.createTransport({
     };
     contactEmail.sendMail(mail, (error) => {
       if (error) {
+        console.log(error)
         res.json({ status: "ERROR" });
       } else {
         res.json({ status: "Message Sent" });
